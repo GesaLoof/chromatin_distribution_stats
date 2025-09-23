cd07f913e1d8ba4687253bb0274537e0b2c9ab70
GesaLoof <gesaloof@gmail.com>
2025-09-23 09:45:46 +0200

    Updated config.py

--- src/chromatin_distribution_stats/config.py
+++ src/chromatin_distribution_stats/config.py
@@ -7,15 +7,26 @@ sample_frac = 0.1
 random_state = 0
 clean_iters = 2  # number of opening+closing passes for speckle cleanup  (0 to disable)
 return_semantic = False  # If True returns a semantic mask 0=background, 1=heterochromatin, 2=non-heterochromatin
-input_im_path = "data/raw/em_image.tif"
-input_mask_path = "data/processed/nucleus_mask.tif"
-output_het_mask_path = "data/processed/het_mask.tif"
-output_labels_path = "data/processed/cluster_labels.tif"
-output_model_path = "data/processed/kmeans_model.pkl"
+# input_im_path = "data/raw/em_image.tif"
+# input_mask_path = "data/processed/nucleus_mask.tif"
+# output_het_mask_path = "data/processed/het_mask.tif"
+# output_labels_path = "data/processed/cluster_labels.tif"
+# output_model_path = "data/processed/kmeans_model.pkl"
+
+
+#batch process k means parameters
+input_image_dir = "data/raw/"
+output_mask_dir = "data/processed/"
 
 
 
 # calculate distribution metrics parameters
+input_mask_dir = "data/processed/"
+input_het_mask_dir = "data/processed/"
+# optional previous dataframe to append to
+# prev_dataframe = pd.read_csv("data/processed/het_metrics.csv")
+# save_df = True  # whether to save the dataframe to output_metrics_path
+# output paths
 n_bins = 20         # bins for radial profiles (0-1 normalized radius)
 outer_width = 0.20  # fraction of radius for outer shell (for radial profile)
 inner_width = 0.20  # fraction of radius for inner shell (for radial profile)
